b98fb07724687719d2cdbfc0c9f7b4fda504bed6
George Golikov <golikov.georgij@mail.ru>
2022-08-25 12:37:22 +0300

    refactoring: split into functions and lambdas

--- src/background-rembg.js
+++ src/background-rembg.js
@@ -1,8 +1,9 @@
 const RM_URL = 'https://api.remove.bg/v1.0/removebg';
 const SERVICE_PAGE_URL = 'https://remove.bg';
 const SERVICE_NAME = 'remove.bg';
-const TOKEN = ''; // google oauth
-// const TOKEN = ''; // mail login
+const IMAGE_SIZE = 'preview';
+// const TOKEN = ''; // google oauth
+const TOKEN = ''; // mail login
 // const TOKEN = ''; // mail georgij login
 // const TOKEN = ''; // mail golikov.gd login
 
@@ -15,43 +16,84 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener(async (imageInfo) => {
-    let data = new FormData();
-    let size = 'preview';
-    data.append('image_url', imageInfo.srcUrl);
-    data.append('size', size);
+    let data = createFormData(imageInfo.srcUrl);
+    let requestConfig = createRequestConfig(data);
+    return getImgFromRequest(requestConfig);
+});
 
-    return fetch(RM_URL, {
+function createFormData(srcUrl) {
+    let data = new FormData();
+    data.append('image_url', srcUrl);
+    data.append('size', IMAGE_SIZE);
+    return data;
+}
+
+function createRequestConfig(formData) {
+    return {
         method: 'POST',
         headers: {
             'accept': 'application/json',
             'X-Api-Key': TOKEN
         },
-        body: data
-    })
-    .then(rawResp => {
-        return rawResp.json()
-    })
-    .then(respBody => {
-        if (respBody?.data) {
-            let binImageB64 = respBody.data.result_b64;
-            let imUrl = 'data:image/png;base64,' + binImageB64;
-            let urlObj = { url: imUrl }
+        body: formData
+    }
+}
 
-            if (size === 'preview') {
-                chrome.tabs.create(urlObj);
-            } else {
-                chrome.downloads.download(urlObj);
-            }
-        } else {
-            let firstErrorMsg = respBody.errors[0].title;
-            let erUrl = 'data:text/html,<h1>' +
-                '<a href="' + SERVICE_PAGE_URL + '">' +
-                SERVICE_NAME + '</a>: ' +
-                firstErrorMsg +
-                '</h1>';
+function getImgFromRequest(requestConfig) {
+    return fetch(RM_URL, requestConfig)
+        .then(getResponseInJson)
+        .then(showResponse)
+        .catch(printFetchErr);
+}
 
-            chrome.tabs.create({ url: erUrl });
-        }
-    })
-    .catch(err => { console.log(err.message) });
-});
+let getResponseInJson = (rawResp) => { return rawResp.json(); };
+let showResponse = (respBody) => {
+    let respData = respBody?.data;
+    if (respData) {
+        showImgWithDeletedBack(respData);
+    } else {
+        showError(respBody);
+    }
+};
+let printFetchErr = (err) => { console.log(err.message); };
+
+function showImgWithDeletedBack(respData) {
+    let urlObj = createImgUrlObjFromBase64(respData.result_b64);
+    if (IMAGE_SIZE === 'preview') {
+        openUrlInNewTab(urlObj);
+    } else {
+        downloadByUrl(urlObj);
+    }
+}
+
+function createImgUrlObjFromBase64(binImageB64) {
+    let imUrl = 'data:image/png;base64,' + binImageB64;
+    return { url: imUrl };
+}
+
+function openUrlInNewTab(urlObj) {
+    chrome.tabs.create(urlObj);
+}
+
+function downloadByUrl(urlObj) {
+    chrome.downloads.download(urlObj);
+}
+
+function showError(respBody) {
+    let firstErrMsg = getFirstErrMsgFromResponse(respBody);
+    let urlObj = createErrUrlObjFromMsg(firstErrMsg);
+    openUrlInNewTab(urlObj);
+}
+
+function getFirstErrMsgFromResponse(respBody) {
+    return respBody.errors[0].title;
+}
+
+function createErrUrlObjFromMsg(errMsg) {
+    let errUrl = 'data:text/html,<h1>' +
+        '<a href="' + SERVICE_PAGE_URL + '">' +
+        SERVICE_NAME + '</a>: ' +
+        errMsg +
+        '</h1>';
+    return { url: errUrl };
+}
--- src/background.js
+++ src/background.js
@@ -2,7 +2,8 @@ const RM_URL = 'https://api.removal.ai/3.0/remove';
 const SERVICE_PAGE_URL = 'https://removal.ai';
 const SERVICE_NAME = 'removal.ai';
 // const TOKEN = ''; // google oauth
-const TOKEN = ''; // mail login
+// const TOKEN = ''; // mail login
+const TOKEN = ''; // mail georgij login
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -13,54 +14,111 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener(async (imageInfo) => {
-  let data = new FormData();
-  data.append('image_url', imageInfo.srcUrl);
-  data.append('get_base64', '1');
+  let data = createFormData(imageInfo.srcUrl);
+  let requestConfig = createRequestConfig(data);
+  return getImgFromRequest(requestConfig);
+});
 
-  return fetch(RM_URL, {
+function createFormData(srcUrl) {
+  let data = new FormData();
+  data.append('image_url', srcUrl);
+  data.append('get_base64', '1');
+  return data;
+}
+
+function createRequestConfig(formData) {
+  return {
     method: 'POST',
     headers: {
       'accept': 'application/json',
       'Rm-Token': TOKEN
     },
-    body: data
-  })
-  .then(rawResp => {
-    return rawResp.json()
-  })
-  .then(respBody => {
-    if (respBody?.base64) {
-      let urlObj;
-      if (respBody?.high_resolution) {
-        urlObj = { url: respBody.high_resolution }
+    body: formData
+  }
+}
 
-        chrome.downloads.download(urlObj);
-      }
+function getImgFromRequest(requestConfig) {
+  return fetch(RM_URL, requestConfig)
+      .then(getResponseInJson)
+      .then(showResponse)
+      .catch(printFetchErr);
+}
 
-      let binImageB64 = respBody.base64;
-      let imUrl = 'data:image/png;base64,' + binImageB64;
-      urlObj = { url: imUrl }
+let getResponseInJson = (rawResp) => { return rawResp.json(); };
+let showResponse = (respBody) => {
+  let highResUrl = respBody?.high_resolution;
+  let binImageB64 = respBody?.base64;
 
-      chrome.tabs.create(urlObj);
-    } else {
-      let errorMsg = respBody.message;
-      let erUrl = 'data:text/html,<h1>' +
-          '<a href="' + SERVICE_PAGE_URL + '">' +
-          SERVICE_NAME + '</a>: ' +
-          errorMsg +
-          '</h1>';
+  if (highResUrl) {
+    downloadHighResImgWithDeletedBack(highResUrl);
+  } else if (binImageB64) {
+    openImgWithDeletedBack(binImageB64);
+  } else {
+    showError(respBody);
+  }
+};
+let printFetchErr = (err) => { showFetchError(err); };
 
-      chrome.tabs.create({ url: erUrl });
-    }
-  })
-  .catch(err => {
-    console.log(err.message)
+function downloadHighResImgWithDeletedBack(highResUrl) {
+  let urlObj = createUrlObjFromUrl(highResUrl);
+  downloadByUrl(urlObj);
+}
 
-    let adviceMsg = 'Perhaps this image is not available through the given link. ' +
-        'Try to open the image in the <b>original</b> mode. Then delete the background once again.';
-    let erUrl = 'data:text/html,<h1>' + err.message + '</h1>' +
-        '<p>' + adviceMsg + '</p>';
+function createUrlObjFromUrl(urlRaw) {
+  return { url: urlRaw };
+}
 
-    chrome.tabs.create({ url: erUrl });
-  });
-});
+function downloadByUrl(urlObj) {
+  chrome.downloads.download(urlObj);
+}
+
+function openImgWithDeletedBack(binImageB64) {
+  let urlObj = createImgUrlObjFromBase64(binImageB64);
+  openUrlInNewTab(urlObj);
+}
+
+function createImgUrlObjFromBase64(binImageB64) {
+  let imUrl = 'data:image/png;base64,' + binImageB64;
+  return createUrlObjFromUrl(imUrl);
+}
+
+function openUrlInNewTab(urlObj) {
+  chrome.tabs.create(urlObj);
+}
+
+function showError(respBody) {
+  let errMsg = getErrMsgFromResponse(respBody);
+  let urlObj = createErrUrlObjFromMsg(errMsg);
+  openUrlInNewTab(urlObj);
+}
+
+function getErrMsgFromResponse(respBody) {
+  return respBody.message;
+}
+
+function createErrUrlObjFromMsg(errMsg) {
+  let errUrl = 'data:text/html,<h1>' +
+      '<a href="' + SERVICE_PAGE_URL + '">' +
+      SERVICE_NAME + '</a>: ' +
+      errMsg +
+      '</h1>';
+  return createUrlObjFromUrl(errUrl);
+}
+
+function showFetchError(err) {
+  console.log(err.message)
+  let adviceMsg = createAdviceMsgForErrPage();
+  let urlObj = createErrUrlObjFromErrAndAdviceMsgs(err.message, adviceMsg);
+  openUrlInNewTab(urlObj);
+}
+
+function createAdviceMsgForErrPage() {
+  return 'Perhaps this image is not available through the given link. ' +
+      'Try to open the image in the <b>original</b> mode. Then delete the background once again.';
+}
+
+function createErrUrlObjFromErrAndAdviceMsgs(errMsg, adviceMsg) {
+  let errUrl = 'data:text/html,<h1>' + errMsg + '</h1>' +
+      '<p>' + adviceMsg + '</p>';
+  return createUrlObjFromUrl(errUrl);
+}
